c24a482828fa47f4e2cf0892a0f4d0d4271c9a36
Francisco Gimenez <dc.francisco@gmail.com>
2014-10-03 11:45:30 +0200

    New model interaction with editor.js

--- examples/create.lua
+++ examples/create.lua
@@ -12,8 +12,42 @@ model.arc_type [tostring (model.arc_type)] = true
 
 model.p1 = model.place_type * {
   name  = "p1",
+  token = 1,
+  [Tag.POSITION   ] = "0,0",
+  [Tag.SELECTED   ] = false,
+  [Tag.HIGHLIGHTED] = true,
+  [Tag.INSTANCE   ] = true,
+}
+
+model.t1 = model.transition_type * {
+  name  = "t1",
+  [Tag.POSITION   ] = "100,100",
+  [Tag.SELECTED   ] = false,
+  [Tag.HIGHLIGHTED] = false,
+  [Tag.INSTANCE   ] = true,
+}
+
+model.p2 = model.place_type * {
+  name  = "p2",
+  token = 1,
+  [Tag.POSITION   ] = "100,150",
+  [Tag.SELECTED   ] = false,
+  [Tag.HIGHLIGHTED] = false,
+  [Tag.INSTANCE   ] = true,
+}
+
+model.t2 = model.transition_type * {
+  name  = "t2",
+  [Tag.POSITION   ] = "-100,100",
+  [Tag.SELECTED   ] = false,
+  [Tag.HIGHLIGHTED] = false,
+  [Tag.INSTANCE   ] = true,
+}
+
+model.p3 = model.place_type * {
+  name  = "p3",
   token = 0,
-  [Tag.POSITION   ] = "100:45",
+  [Tag.POSITION   ] = "-100, 150",
   [Tag.SELECTED   ] = false,
   [Tag.HIGHLIGHTED] = false,
   [Tag.INSTANCE   ] = true,
@@ -21,9 +55,24 @@ model.p1 = model.place_type * {
 
 model.a1 = model.arc_type * {
   source = model.p1,
-  target = model.p1,
+  target = model.t1,
   [Tag.INSTANCE] = true,
 }
 
-Data.clear (model.a1)
-Data.clear (model.p1)
+model.a2 = model.arc_type * {
+  source = model.t1,
+  target = model.p2,
+  [Tag.INSTANCE] = true,
+}
+
+model.a3 = model.arc_type * {
+  source = model.p1,
+  target = model.t2,
+  [Tag.INSTANCE] = true,
+}
+
+model.a4 = model.arc_type * {
+  source = model.t2,
+  target = model.p3,
+  [Tag.INSTANCE] = true,
+}
